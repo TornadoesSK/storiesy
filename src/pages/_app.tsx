394c584d36e1e1924c487e9f5f15b35e280da788
Daniel Forrai <forrai@jednanula.sk>
2023-04-01 16:02:35 +0200

    drawer menu

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,12 @@ import { type AppType } from "next/app";
 import Head from "next/head";
 import { api } from "../utils/api";
 import "../styles/globals.css";
-import Link from "next/link";
 import { appStateContext, useApp } from "../components/useAppState";
 import { useRouter } from "next/router";
 import { useCallback } from "react";
+import DrawerSide from "../components/nav/DrawerSide";
+
+const sidebarId = "sidebar-menu";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
 	const appState = useApp();
@@ -18,45 +20,23 @@ const MyApp: AppType = ({ Component, pageProps }) => {
 	return (
 		<>
 			<main>
-				<div className="navbar bg-base-100">
-					<div className="navbar-start">
-						<div className="dropdown">
-							<label tabIndex={0} className="btn-ghost btn-circle btn">
-								<MenuIcon />
-							</label>
-							<ul
-								tabIndex={0}
-								className="dropdown-content menu rounded-box menu-compact mt-3 w-52 bg-base-100 p-2 shadow"
-							>
-								<li>
-									<Link href="/">Expenses</Link>
-								</li>
-								<li>
-									<Link href="/categories">Categories</Link>
-								</li>
-							</ul>
-						</div>
-					</div>
-					<div className="navbar-center">
-						<div className="text-xl font-medium normal-case">Moneyger</div>
-					</div>
-					<div className="navbar-end">
-						<button className="btn-ghost btn-circle btn">
-							<SearchIcon />
-						</button>
-					</div>
-				</div>
 				<Head>
-					<title>Moneyger</title>
-					<meta name="description" content="Expense tracker" />
+					<title>Storiesy</title>
+					<meta name="description" content="Comics creation using AI" />
 					<link rel="icon" href="/favicon.ico" />
 				</Head>
-				<appStateContext.Provider value={appState}>
-					<button onClick={handleSignOutClick} className="btn">
-						Sign out
-					</button>
-					<Component {...pageProps} />
-				</appStateContext.Provider>
+				<div className="drawer-mobile drawer">
+					<input id={sidebarId} type="checkbox" className="drawer-toggle" />
+					<div className="drawer-content flex flex-col items-center justify-center bg-white">
+						<label htmlFor={sidebarId} className="btn-primary drawer-button btn lg:hidden">
+							Open drawer
+						</label>
+						<appStateContext.Provider value={appState}>
+							<Component {...pageProps} />
+						</appStateContext.Provider>
+					</div>
+					<DrawerSide sidebarId={sidebarId} handleSignOutClick={handleSignOutClick} />
+				</div>
 			</main>
 		</>
 	);
@@ -65,29 +45,12 @@ const MyApp: AppType = ({ Component, pageProps }) => {
 const MenuIcon = () => (
 	<svg
 		xmlns="http://www.w3.org/2000/svg"
-		className="h-5 w-5"
+		className="h-16 w-16"
 		fill="none"
 		viewBox="0 0 24 24"
 		stroke="currentColor"
 	>
 		<path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" />
-	</svg>
-);
-
-const SearchIcon = () => (
-	<svg
-		xmlns="http://www.w3.org/2000/svg"
-		className="h-5 w-5"
-		fill="none"
-		viewBox="0 0 24 24"
-		stroke="currentColor"
-	>
-		<path
-			strokeLinecap="round"
-			strokeLinejoin="round"
-			strokeWidth="2"
-			d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-		/>
 	</svg>
 );
 
